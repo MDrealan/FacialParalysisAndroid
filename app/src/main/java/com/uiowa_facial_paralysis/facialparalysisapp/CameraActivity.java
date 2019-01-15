a0d98fd50424cbf76ffdb019417611c192c0db47
Keil, Anna Y <anna-keil@uiowa.edu>
2019-01-14 20:12:56 -0600

    camera photo and video implementation.

--- app/src/main/java/com/uiowa_facial_paralysis/facialparalysisapp/CameraActivity.java
+++ app/src/main/java/com/uiowa_facial_paralysis/facialparalysisapp/CameraActivity.java
@@ -1,14 +1,35 @@
 package com.uiowa_facial_paralysis.facialparalysisapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Camera;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceHolder;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 
 public class CameraActivity extends AppCompatActivity {
 
-
+    private CameraDevice mCamera;
+    private SurfaceHolder mHolder;
+    private ImageView mView;
+    private Uri imageUri;
+    private static final int REQUEST_IMAGE_CAPTURE = 101;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +37,6 @@ public class CameraActivity extends AppCompatActivity {
         Button new_photo1 = (Button)findViewById(R.id.front_button);
         Button new_photo2 = (Button)findViewById(R.id.left_button);
         Button new_photo3 = (Button)findViewById(R.id.right_button);
-        Button new_video = (Button)findViewById(R.id.video_button);
         new_photo1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,32 +55,69 @@ public class CameraActivity extends AppCompatActivity {
                 startNewPhoto();
             }
         });
-        new_video.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startNewVideo();
-            }
-        });
-
     }
 
     private void startNewPhoto()
     {
         //verify here
 
-        Intent intent = new Intent(this, CameraActivity2.class);//go to Next activity
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);//go to Next activity
+        imageUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),"fname_" +
+                String.valueOf(System.currentTimeMillis()) + ".jpg"));
+        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, imageUri);
 
-        startActivity(intent);
+        if(intent.resolveActivity(getPackageManager()) != null){
+
+            startActivityForResult(intent,REQUEST_IMAGE_CAPTURE);
+        }
+
+        //getOutputMediaFile(REQUEST_IMAGE_CAPTURE);
     }
 
-    private void startNewVideo()
-    {
-        //verify here
 
-        Intent intent = new Intent(this, CameraActivity3.class);//go to Next activity
 
-        startActivity(intent);
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Log.e("URI",imageUri.toString());
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mView.setImageBitmap(imageBitmap);
+        }
+
     }
+
+    /*private File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "FacialParalysisApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("FacialParalysisApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }*/
+
 
 
 
--- app/src/main/java/com/uiowa_facial_paralysis/facialparalysisapp/CameraActivity2.java
+++ app/src/main/java/com/uiowa_facial_paralysis/facialparalysisapp/CameraActivity2.java
@@ -1,14 +1,21 @@
 package com.uiowa_facial_paralysis.facialparalysisapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.hardware.Camera;
+import android.hardware.camera2.CameraDevice;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.SurfaceHolder;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -20,108 +27,52 @@ import java.util.Date;
 import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 
 public class CameraActivity2 extends AppCompatActivity {
-
-    private Camera mCamera;
-    private CameraPreview mPreview;
-    private static final String TAG = "CameraActivity2";
+    private CameraDevice mCamera;
+    private SurfaceHolder mHolder;
+    private ImageView mView;
+    private Uri imageUri;
+    private static final int REQUEST_VIDEO_CAPTURE = 102;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera2);
-        // Create an instance of Camera
-        mCamera = getCameraInstance();
-
-        // Create our Preview view and set it as the content of our activity.
-        mPreview = new CameraPreview(this, mCamera);
-        /*FrameLayout preview = (FrameLayout) findViewById(R.id.activity_camera2);*/
-        /*preview.addView(mPreview);*/
-        mCamera.open();
-        //mCamera.setPreviewDisplay();
-        mCamera.startPreview();
-        // Add a listener to the Capture button
-        Button captureButton = (Button) findViewById(R.id.button_capture);
-        captureButton.setOnClickListener(new View.OnClickListener()
-        {
+        setContentView(R.layout.activity_camera);
+        Button new_video = (Button) findViewById(R.id.video_button);
+        new_video.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                mCamera.takePicture(null, null, mPicture);
+            public void onClick(View v) {
+                startNewVideo();
             }
         });
     }
-    private boolean checkCameraHardware(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-            // this device has a camera
-            return true;
-        } else {
-            // no camera on this device
-            return false;
+    private void startNewVideo()
+    {
+        //verify here
+
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);//go to Next activity
+        imageUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),"fname_" +
+                String.valueOf(System.currentTimeMillis()) + ".3gp"));
+        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, imageUri);
+
+        if(intent.resolveActivity(getPackageManager()) != null){
+
         }
+        startActivityForResult(intent,REQUEST_VIDEO_CAPTURE);
+
+        //getOutputMediaFile(REQUEST_IMAGE_CAPTURE);
     }
 
-    public static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open(); // attempt to get a Camera instance
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Log.e("URI",imageUri.toString());
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mView.setImageBitmap(imageBitmap);
         }
-        catch (Exception e){
-            // Camera is not available (in use or does not exist)
-        }
-        return c; // returns null if camera is unavailable
+
     }
 
-    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
 
-        @Override
-        public void onPictureTaken(byte[] data, Camera camera) {
-
-            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
-            if (pictureFile == null){
-                Log.d(TAG, "Error creating media file, check storage permissions");
-                return;
-            }
-
-            try {
-                FileOutputStream fos = new FileOutputStream(pictureFile);
-                fos.write(data);
-                fos.close();
-            } catch (FileNotFoundException e) {
-                Log.d(TAG, "File not found: " + e.getMessage());
-            } catch (IOException e) {
-                Log.d(TAG, "Error accessing file: " + e.getMessage());
-            }
-        }
-
-        private File getOutputMediaFile(int type){
-            // To be safe, you should check that the SDCard is mounted
-            // using Environment.getExternalStorageState() before doing this.
-
-            File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                    Environment.DIRECTORY_PICTURES), "FacialParalysisApp");
-            // This location works best if you want the created images to be shared
-            // between applications and persist after your app has been uninstalled.
-
-            // Create the storage directory if it does not exist
-            if (! mediaStorageDir.exists()){
-                if (! mediaStorageDir.mkdirs()){
-                    Log.d("FacialParalysisApp", "failed to create directory");
-                    return null;
-                }
-            }
-
-            // Create a media file name
-            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-            File mediaFile;
-            if (type == MEDIA_TYPE_IMAGE){
-                mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                        "IMG_"+ timeStamp + ".jpg");
-            } else {
-                return null;
-            }
-
-            return mediaFile;
-        }
-    };
 
 }
