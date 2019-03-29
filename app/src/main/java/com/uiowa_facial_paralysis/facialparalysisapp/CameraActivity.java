326ddf485cfb417500bb60bf7a9a1e17b974eed7
Keil, Anna Y <anna-keil@uiowa.edu>
2019-03-29 11:25:27 -0500

    added camera activity page in the manifest. removed the video and included photos only for testing.

--- app/src/main/java/com/uiowa_facial_paralysis/facialparalysisapp/CameraActivity.java
+++ app/src/main/java/com/uiowa_facial_paralysis/facialparalysisapp/CameraActivity.java
@@ -9,6 +9,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.SurfaceHolder;
@@ -21,7 +22,6 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -30,6 +30,8 @@ public class CameraActivity extends AppCompatActivity {
     private ImageView mView;
     private Uri imageUri;
     private static final int REQUEST_IMAGE_CAPTURE = 101;
+    private String currentPhotoPath;
+    private static final int REQUEST_TAKE_PHOTO = 1;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,61 +40,44 @@ public class CameraActivity extends AppCompatActivity {
         new_Fphoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startNewPhoto();
+                dispatchTakePictureIntent();
             }
         });
         Button new_Lphoto = (Button)findViewById(R.id.left_button);
         new_Lphoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startNewPhoto();
+                dispatchTakePictureIntent();
             }
         });
         Button new_Rphoto = (Button)findViewById(R.id.right_button);
         new_Rphoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startNewPhoto();
+                dispatchTakePictureIntent();
             }
         });
         Button new_Pphoto = (Button)findViewById(R.id.pucker_button);
         new_Pphoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startNewPhoto();
+                dispatchTakePictureIntent();
             }
         });
         Button new_FFphoto = (Button)findViewById(R.id.frontface_button);
         new_FFphoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startNewPhoto();
+                dispatchTakePictureIntent();
             }
         });
-        Button new_video = (Button)findViewById(R.id.video_button);
+        /*Button new_video = (Button)findViewById(R.id.video_button);
         new_video.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goToVideo();
             }
-        });
-    }
-
-    private void startNewPhoto()
-    {
-        //verify here
-
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);//go to Next activity
-        imageUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),"fname_" +
-                String.valueOf(System.currentTimeMillis()) + ".jpg"));
-        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, imageUri);
-
-        if(intent.resolveActivity(getPackageManager()) != null){
-
-            startActivityForResult(intent,REQUEST_IMAGE_CAPTURE);
-        }
-
-        //getOutputMediaFile(REQUEST_IMAGE_CAPTURE);
+        });*/
     }
 
 
@@ -109,11 +94,71 @@ public class CameraActivity extends AppCompatActivity {
 
     }
 
+
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
     private void goToVideo()
     {
         Intent intent = new Intent(this, CameraActivity2.class); //go to Next activity
         startActivity(intent);
     }
+
+    /*private void startNewPhoto()
+    {
+        //verify here
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);//go to Next activity
+        imageUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),"fname_" +
+                String.valueOf(System.currentTimeMillis()) + ".jpg"));
+        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, imageUri);
+
+        if(intent.resolveActivity(getPackageManager()) != null){
+
+            startActivityForResult(intent,REQUEST_IMAGE_CAPTURE);
+        }
+
+        //getOutputMediaFile(REQUEST_IMAGE_CAPTURE);
+    }*/
 
 
     /*private File getOutputMediaFile(int type){
