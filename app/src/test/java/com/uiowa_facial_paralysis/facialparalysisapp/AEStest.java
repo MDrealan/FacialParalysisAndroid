28c6c1f69ea0569a43e214ecd73d721a397f6a78
MDrealan <markus-drealan@uiowa.edu>
2019-04-02 16:37:08 -0500

    more updating with testing

---
+++ app/src/test/java/com/uiowa_facial_paralysis/facialparalysisapp/AEStest.java
@@ -0,0 +1,18 @@
+package com.uiowa_facial_paralysis.facialparalysisapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class AEStest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
